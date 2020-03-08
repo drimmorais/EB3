305e78694120077487a53fce7817595f72974c72
Dri Morais <Adrielle>
2020-03-08 18:04:16 -0300

    finzinho

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Entre com a idade: ");
+		int idade = entrada.nextInt();
+		
+		if(idade > 20) {
+			System.out.println("Idade maior que 20");
+		}else if(idade > 12 && idade < 17) {
+			System.out.println("Idade maior que 12 e menor que 17");
+		}else if(idade == 7 || idade < 5) {
+			System.out.println("Idade igual a 7 OU menor que 5");
+		}else {
+			System.out.println("Outras combinações");
+		}
 
 	}
 
